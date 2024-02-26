a9577c6ba2c5517431bccdb7def594f3f76b20d6
alexbulgakov <contact.bulgakov@gmail.com>
2024-02-27 03:47:31 +0700

    feat: add new types

--- index.ts
+++ index.ts
@@ -1,8 +1,13 @@
 import "dotenv/config";
-
+import { CreateWebSocketServer } from "./src/websocket_server";
 import { httpServer } from "./src/http_server";
 
-const HTTP_PORT = process.env.HTTP_PORT || 8181;
+const HTTP_PORT: number = Number(process.env.HTTP_PORT) || 8181;
+const WEB_SOCKET_PORT: number = Number(process.env.WEB_SOCKET_PORT) || 4000;
 
-console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+new CreateWebSocketServer(WEB_SOCKET_PORT);
+
+console.log(
+  `Start static http server on the ${HTTP_PORT} port and websocket server on the ${WEB_SOCKET_PORT} port.`
+);
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,32 @@
-export interface User {
-  id?: string;
-  name: string;
-  password: string;
+import { TYPES } from "../constants";
+
+export interface RegistrationRequest {
+  type: typeof TYPES.REG;
+  data: {
+    name: string;
+    password: string;
+  };
+  id: number;
+}
+
+export interface RegistrationResponse {
+  type: typeof TYPES.REG;
+  data: {
+    name: string;
+    index: number;
+    error: boolean;
+    errorText: string;
+  };
+  id: number;
+}
+
+export interface UpdateWinners {
+  type: typeof TYPES.UPDATE_WINNERS;
+  data: [
+    {
+      name: string;
+      password: string;
+    }
+  ];
+  id: number;
 }
