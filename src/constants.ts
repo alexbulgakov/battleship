1b4505dc653d8a9419ee9e38ad418425d55ca18a
alexbulgakov <contact.bulgakov@gmail.com>
2024-02-27 04:23:56 +0700

    feat: update constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,27 +1,24 @@
-export const TYPES = {
-  REG: "reg",
-  UPDATE_WINNERS: "update_winners",
-  CREATE_ROOM: "create_room",
-  ADD_USER_TO_ROOM: "add_user_to_room",
-  CREATE_GAME: "create_game",
-  UPDATE_ROOM: "update_room",
-  ADD_SHIPS: "add_ships",
-  START_GAME: "start_game",
-  ATTACK: "attack",
-  RANDOM_ATTACK: "randomAttack",
-  TURN: "turn",
-  FINISH: "finish",
-};
+export enum TYPES {
+  REG = "reg",
+  UPDATE_WINNERS = "update_winners",
+  CREATE_ROOM = "create_room",
+  ADD_USER_TO_ROOM = "add_user_to_room",
+  CREATE_GAME = "create_game",
+  UPDATE_ROOM = "update_room",
+  ADD_SHIPS = "add_ships",
+  START_GAME = "start_game",
+  ATTACK = "attack",
+  RANDOM_ATTACK = "randomAttack",
+  TURN = "turn",
+  FINISH = "finish",
+}
 
-export const SHIP_TYPES = {
-  SINGLE_DECK: "single-deck",
-  TWO_DECK: "two-deck",
-  THREE_DECK: "three-deck",
-  FOUR_DECK: "four-deck",
-};
+export type SHIP_TYPES = "small" | "medium" | "large" | "huge";
 
-export const ACTION_TYPES = {
-  KILLED: "killed",
-  HIT: "hit",
-  MISSED: "missed",
-};
+export type GAME_TYPES = "waiting" | "in-progress" | "finished";
+
+export enum SHIP_ACTION_TYPES {
+  KILL = "kill",
+  SHOT = "shot",
+  MISS = "miss",
+}
